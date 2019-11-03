10a56c25fa79fe7434f200acf9090a93951d6173
Parth <parthx@live.com>
2019-11-02 23:10:20 -0700

    New feature branch is added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I'm changed.
+          I'm changed at react-feature branch.
         </p>
         <a
           className="App-link"
